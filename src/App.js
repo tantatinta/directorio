8bbce8fda5dcbece5ff6688a0a229f402de80089
tantatinta <bluekasia@gmail.com>
2020-04-21 18:00:44 -0400

    dynamic search working with kinks

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
-import React, {useState} from "react";
-import Header from "./components/Header/Header";
+import React, {useState, useEffect} from "react";
 import users from "./utils/users.json"
-import EmployeeWrapper from "./components/EmployeeWrapper/EmployeeWrapper";
+import EmployeeWrapper from "./components/EmployeeWrapper";
 
 
 function App() {  
-  const [theUsers, setTheUsers]=useState(users)
-  const [masterList, setMasterList]=useState(users)
-  const [searchTerm, setSearchTerm]=useState("")
-  const handleSearchChange=(term)=>{
-    console.log(term)
-    setSearchTerm(term)
+  const [theUsers, setTheUsers] = useState([]);  
+  const [searchTerm, setSearchTerm] = useState("");
+  
+  
+  useEffect(() => {
+    setTheUsers(users)
+  }, [])
+
+  const handleSearchChange = e => {
+    setSearchTerm(e.target.value);
+    
+    const filteredUsersArrayByName = users.filter(user => user.name.first.toLowerCase().includes(searchTerm.toLowerCase()))
+    setTheUsers(filteredUsersArrayByName);
+
+    const filteredUsersArrayByLast = users.filter(user => user.name.last.toLowerCase().includes(searchTerm.toLowerCase()))
+    setTheUsers(filteredUsersArrayByLast);
   }
+
+  
   return (
     <div>
-    <Header searchTerm ={searchTerm} handler={handleSearchChange}/>
-    <EmployeeWrapper users={theUsers} searchTerm={searchTerm}/>
+      <header className="header container bg-dark text-light mb-3 pb-2">
+        <h1>Employee Directory</h1>
+        <input 
+          type="search" 
+          placeholder="search employee"
+          className="form-control-plaintext bg-light mb-2" 
+          name="searchInput"            
+          onChange={handleSearchChange}/>
+      </header>
+      <EmployeeWrapper 
+      users={theUsers}
+      />
     </div>
   );
 }
